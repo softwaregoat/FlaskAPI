16cbd70be7f176557dd26ce27badec428ef2a485
Software Goat <61810640+softwaregoat@users.noreply.github.com>
2020-12-02 21:13:28 +0800

    added api key

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ from os import path
 app = Flask(__name__)
 FilePath = './Files/'
 
+f = open('api_key.json',)
+data = json.load(f)
+api_key = data['api_key']
+f.close()
+
 
 @app.route('/')
 def hello_world():
@@ -13,37 +18,37 @@ def hello_world():
 
 @app.route('/api/<string:file_name>', methods=['GET'])
 def get_query(file_name):
-    key = request.args['key']
-    val = ''
-    if len(key) == 0:
+    key = request.args['api_key']
+    if key != api_key:
         abort(404)
     file_name = FilePath + file_name + '.txt'
     if path.exists(file_name):
+        result = ''
         with open(file_name) as f:
-            for line in f:
-                lines = line.replace('\'', '').split(',')
-                for l in lines:
-                    (k, v) = l.split(':')
-                    if k == key:
-                        val = v
-                        break
-                if val != '':
-                    break
+            result = f.read()
         f.close()
-        if val == '':
+        if result == '':
             return 'No result', 200
-        return jsonify({'key': key, 'result': val}), 200
+        return result, 200
     else:
-        abort(400)
+        return 'No exit the file', 200
 
 
 @app.route('/api/uploader', methods=['GET', 'POST'])
 def upload_file():
+    key = request.args['api_key']
+    if key != api_key:
+        abort(404)
     if request.method == 'POST':
-        file = request.files['file']
-        filename = secure_filename(file.filename)
-        file.save(path.join(FilePath, filename))
-        return 'file uploaded successfully'
+        if 'files[]' not in request.files:
+            return 'request param should be files[]', 400
+        files = request.files.getlist('files[]')
+        for file in files:
+            filename = secure_filename(file.filename)
+            file.save(path.join(FilePath, filename))
+        return 'files uploaded successfully', 200
+    else:
+        return 'It should POST', 400
    
 
 if __name__ == '__main__':
